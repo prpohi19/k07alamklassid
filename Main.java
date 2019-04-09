ead834b370d59938bdd5bc4f6e0f22faf19e6f3d
Karen Minski <43170840+karenminski@users.noreply.github.com>
2019-04-09 22:19:34 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 public class Main{
     public static void main(String[] args){
-      Pet a1 = new Dog(7, 5, "Matu");
-      Pet a2 = new Cat(5, 4, "Liisu");
+      Pet p1 = new Dog(7, 5, "Matu");
+      Pet p2 = new Cat(5, 4, "Liisu");
       Pet[] shelter = {new Dog(11, 2, "Rex"), new Cat(3, 7, "Ruudi"), new Cat(6, 2, "Aadu")};
-      System.out.println(a1.Hello());
-      System.out.println(a1.Age());
+      System.out.println(p1.Hello());
+      System.out.println(p1.Age());
       System.out.println("****************");
-      System.out.println(a2.Hello());
-      System.out.println(a2.Age());
+      System.out.println(p2.Hello());
+      System.out.println(p2.Age());
       System.out.println("****************");
       System.out.println("Varjupaigas on " + shelter.length + " looma!");
     }
   }
+
+/* 
+Mina olen koer Matu ja minu sabapikkus on 7.0 cm!
+Ma olen 5 aastane!
+****************
+Mina olen kass Liisu ja minu sabapikkus on 5.0 cm!
+Ma olen 4 aastane!
+****************
+Varjupaigas on 3 looma!
+*/
