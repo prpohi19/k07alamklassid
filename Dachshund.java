130de7fdcb592a3668ae4d61149989f8c60c1369
Taavi Liivat <43164710+taavlii@users.noreply.github.com>
2019-05-09 10:30:57 +0300

    seitsmes kodune valmis

---
+++ Dachshund.java
@@ -0,0 +1,6 @@
+class Dachshund extends Dog {
+    public Dachshund(String name, int weight, int speed){
+        super(name, weight,speed);
+        description = "I look like a sausage";
+    }
+}
---
+++ Shiba.java
@@ -0,0 +1,8 @@
+class Shiba extends Dog {
+
+    public Shiba(String name, int weight, int speed){
+        super(name, weight,speed);
+          description = "I am doge";
+    }
+
+}
